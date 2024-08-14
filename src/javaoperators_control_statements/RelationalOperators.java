fff19d1290588b3105c7d7aa87662843f8dbe8d3
shahbazkhan520441 <shahbazkhan520441@gmail.com>
2024-08-14 16:40:26 +0530

    relationaloperators

---
+++ src/javaoperators_control_statements/RelationalOperators.java
@@ -0,0 +1,16 @@
+package javaoperators_control_statements;
+
+public class RelationalOperators {
+//	The following simple example program demonstrates the relational operators.
+	
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 20;
+		System.out.println("a == b = " + (a == b) );
+		System.out.println("a != b = " + (a != b) );
+		System.out.println("a > b = " + (a > b) );
+		System.out.println("a < b = " + (a < b) );
+		System.out.println("b >= a = " + (b >= a) );
+		System.out.println("b <= a = " + (b <= a) );	
+	}
+}
