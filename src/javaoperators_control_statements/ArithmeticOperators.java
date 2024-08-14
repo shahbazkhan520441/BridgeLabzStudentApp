5c6a5fb4c90b0096b55396b179d3cce23c4855cc
shahbazkhan520441 <shahbazkhan520441@gmail.com>
2024-08-14 16:34:08 +0530

    arithematic operator

---
+++ src/javaoperators_control_statements/ArithmeticOperators.java
@@ -0,0 +1,28 @@
+package javaoperators_control_statements;
+
+public class ArithmeticOperators {
+	//Arithmetic operators are used in mathematical expressions in the same way that they are used in algebra. The
+	//	following table lists the arithmetic operators. Assume integer variable A holds 10 and variable B holds 20, then:
+
+	public static void main(String[] args) {
+
+		int a = 10;
+		int b = 20;
+		int c = 25;
+		int d = 25;
+		System.out.println("a + b = " + (a + b) );
+		System.out.println("a - b = " + (a - b) );
+		System.out.println("a * b = " + (a * b) );
+		System.out.println("b / a = " + (b / a) );
+		System.out.println("b % a = " + (b % a) );
+		System.out.println("c % a = " + (c % a) );
+		System.out.println("a++ = " + (a++) );
+		System.out.println("b-- = " + (a--) );
+		// Check the difference in d++ and ++d
+		System.out.println("d++ = " + (d++) );
+		System.out.println("++d = " + (++d) );
+
+
+
+	}
+}
