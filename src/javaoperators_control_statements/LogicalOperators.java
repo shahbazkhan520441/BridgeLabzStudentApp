77d70eaa89cbbca4c0eb9f22a7cf858b3da4f24e
shahbazkhan520441 <shahbazkhan520441@gmail.com>
2024-08-14 17:15:46 +0530

    logical operators

---
+++ src/javaoperators_control_statements/LogicalOperators.java
@@ -0,0 +1,15 @@
+package javaoperators_control_statements;
+
+public class LogicalOperators {
+	//	The following simple example program demonstrates the logical operators
+	public static void main(String args[]) {  
+
+		boolean a = true;  
+		boolean b = false;  
+
+		System.out.println("a && b = " + (a&&b));  
+		System.out.println("a || b = " + (a||b) );  
+		System.out.println("!(a && b) = " + !(a && b));  
+	}  
+
+}
